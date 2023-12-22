d10f7b4aba4f3429b929d28d75fc9766843edff9
baishali2502 <baishali2502@gmail.com>
2023-12-23 01:05:39 +0530

    Test:Unpark a car

--- ParkingLot/src/test/java/bridgelabz/com/parkinglot/ParkingLotTest.java
+++ ParkingLot/src/test/java/bridgelabz/com/parkinglot/ParkingLotTest.java
@@ -29,4 +29,25 @@ public class ParkingLotTest {
         assertTrue("Car should be parked successfully", result);
         assertEquals("Parking lot should have one parked car", 1, parkingLot.getParkedCarsCount());
     }
+    /**
+     * Test case: Unpark a Car
+     *
+     * @desc Ensures that a driver can successfully unpark their car from the parking lot.
+     * @params ParkingLot with a parked car and a mocked Car instance.
+     * @returns True if the car is unparked successfully, false if the car is not found.
+     */
+    @Test
+    public void testUnparkCar() {
+        // Arrange
+        ParkingLot parkingLot = new ParkingLot(1);
+        Car car = mock(Car.class);
+        parkingLot.parkCar(car);
+
+        // Act
+        boolean result = parkingLot.unparkCar(car);
+
+        // Assert
+        assertTrue("Car should be unparked successfully", result);
+        assertEquals("Parking lot should have no parked cars", 0, parkingLot.getParkedCarsCount());
+    }
 }
